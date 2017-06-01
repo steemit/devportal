74be02b10b38f4e744848b57d59da47d4106de83
Mike Cifani <mcifani@users.noreply.github.com>
2017-06-01 01:41:16 -0400

    Check for resize on collapse

--- js/adjust.js
+++ js/adjust.js
@@ -1,19 +1,23 @@
 function DynamicAlign() {
-    $('section.right-code > p').each(function() {
-        // compute right side
-        var $text = $(this)[0].innerHTML.toLowerCase();
-        var $right_position = $(this).position().top - $(this).parent().parent().offset().top;
+    var $window = $(window);
+    var windowsize = $window.width();
+    if (windowsize > 1000) {
+        $('section.right-code > p').each(function () {
+            // compute right side
+            var $text = $(this)[0].innerHTML.toLowerCase();
+            var $right_position = $(this).position().top - $(this).parent().parent().offset().top;
 
-        // compute left side & match difference
-        var $left_item = $("#" + $text);
-        if ($left_item) {
-            var $left_parent = $left_item.parent().parent().offset().top;
-            var $left_height = $left_item.position().top;
-            var $left_position = $left_height - $left_parent;
-            var $difference = Math.abs($left_position - $right_position);
-            $left_item.css('margin-top', $difference);
-        }
-    });
+            // compute left side & match difference
+            var $left_item = $("#" + $text);
+            if ($left_item) {
+                var $left_parent = $left_item.parent().parent().offset().top;
+                var $left_height = $left_item.position().top;
+                var $left_position = $left_height - $left_parent;
+                var $difference = Math.abs($left_position - $right_position);
+                $left_item.css('margin-top', $difference);
+            }
+        });
+    }
 };
 
 window.onload = DynamicAlign;
