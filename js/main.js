ff267e9f3195d8f7d5ca024e331b244c60f1a9d2
fm <3015990+feruzm@users.noreply.github.com>
2018-04-11 21:13:43 +0300

    wip

--- js/main.js
+++ js/main.js
@@ -138,4 +138,37 @@ function getClosestHeader() {
           }
         }
       });
+        var acc = document.getElementsByClassName("accordion");
+        var i;
+
+        for (i = 0; i < acc.length; i++) {
+            acc[i].addEventListener("click", function() {
+                /* Toggle between adding and removing the "active" class,
+                to highlight the button that controls the panel */
+                this.classList.toggle("active");
+
+                /* Toggle between hiding and showing the active panel */
+                var panel = this.nextElementSibling;
+                if (panel.style.display === "block") {
+                    panel.style.display = "none";
+                } else {
+                    panel.style.display = "block";
+                }
+            });
+        }
+        acc[0].click();
+        var clicked = false;
+
+        $(window).scroll(function (event) {
+            var scroll = $(window).scrollTop();
+            console.log(scroll);
+            var quickS = $('#quickstart-choose-library').offset().top;
+            console.log(quickS);
+            
+            if (scroll > quickS && !clicked) {
+                clicked = true;
+                acc[0].click();
+                acc[1].click();
+            }
+        });
     });
