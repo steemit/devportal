7ba1d9e9ffffaaa15435231cb9b3cb179798c4d9
Anthony Martin <github@martin-studio.com>
2018-06-21 13:08:22 -0700

    now using public repo #226

--- lib/scrape/tutorials_job/python.rb
+++ lib/scrape/tutorials_job/python.rb
@@ -4,9 +4,7 @@ module Scrape
   module TutorialsJob
     class Python < Base
       def initialize(options = {})
-        # FIXME Need to make the repo public (workaround as follows).
-        # options[:tutorial_github_url] = 'https://github.com/steemit/devportal-tutorials-py.git'
-        options[:tutorial_github_url] ||= 'git@github.com:steemit/devportal-tutorials-py.git'
+        options[:tutorial_github_url] = 'https://github.com/steemit/devportal-tutorials-py.git'
         options[:tutorial_github_name] ||= 'devportal-tutorials-py'
         options[:tutorial_url] = 'https://github.com/steemit/devportal-tutorials-py'
         options[:dest_tutorials_path] ||= '_tutorials-python'
