4165a096b25f91e9d8459bfd9682df1305cbc48d
Anthony Martin <github@martin-studio.com>
2019-01-09 12:12:33 -0800

    updated post generator plugin to account for certain markdown interpretor differences

--- _plugins/post_generator.rb
+++ _plugins/post_generator.rb
@@ -12,7 +12,30 @@ module Jekyll
       api = Steem::CondenserApi.new
       
       api.get_content(author, permlink) do |content|
-        content.body + <<~DONE
+        body = content.body
+        
+        # This will normalize image hoster proxy URLs that the author copied
+        # from another post.
+        
+        body = body.gsub(/https:\/\/steemitimages.com\/[0-9]+x0\/https:\/\//, 'https://')
+        
+        # Although it works on steemit.com and many other markdown interpretors,
+        # kramdown doesn't like this, so we have to fix it:
+        # 
+        # <div>
+        #   This *won't* work.
+        # </div>
+        #
+        # See: https://stackoverflow.blog/2008/06/25/three-markdown-gotcha/
+        
+        body = body.gsub(/<([^\/].+)>(.+)<\/\1>/m) do
+          match = Regexp.last_match
+          html = Kramdown::Document.new(match[2]).to_html
+          
+          "<#{match[1]}>#{html.gsub("\n", "<br />")}</#{match[1]}>"
+        end
+        
+        body + <<~DONE
         \n<hr />
         <p>
           See: <a href="https://steemit.com/@#{author}/#{permlink}">#{content.title}</a>
