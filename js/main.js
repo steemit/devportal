e5089e2ee5aa51709e5e2e09a6a1f1da900496d8
Anthony Martin <github@martin-studio.com>
2020-01-06 13:08:17 -0800

    added turbolinks

--- js/main.js
+++ js/main.js
@@ -100,8 +100,7 @@ function getClosestHeader() {
 }
 
 
-
-$(document).ready(function(){
+$(document).on('ready turbolinks:load', function() {
     // Select all links with hashes
     $('a[href*="#"]')
     // Remove links that don't actually link to anything
